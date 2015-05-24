c44dcfb912de0a6716fa1d13e46ca6972c449d9c
Daniel Eder <daniel@deder.at>
2015-05-25 01:52:10 +0200

    fixed maven structure

---
+++ src/test/java/org/jrune/test/RuneEntityTest.java
@@ -0,0 +1,14 @@
+package org.jrune.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RuneEntityTest {
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
