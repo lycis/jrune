68df7c019d8a6e97ae9a625cb6b841696527386f
Daniel Eder <daniel@deder.at>
2015-05-25 02:02:29 +0200

    #7 test for setProperty with string

--- src/test/java/org/jrune/test/RuneEntityTest.java
+++ src/test/java/org/jrune/test/RuneEntityTest.java
@@ -2,13 +2,24 @@ package org.jrune.test;
 
 import static org.junit.Assert.*;
 
+import org.jrune.core.RuneEngine;
+import org.jrune.entity.RuneEntity;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RuneEntityTest {
+    private RuneEngine engine = null;
+    
+    @Before
+    public void before() {
+	engine = new RuneEngine();
+    }
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void testSetPropertyString() {
+	RuneEntity entity = new RuneEntity(engine);
+	entity.setProperty("string", "test-value");
+	assertEquals("test-value", entity.getProperty("string"));
     }
 
 }
