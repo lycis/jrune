f7bf68b57f9bf3b3647e6f850f163432e7a2e2eb
Daniel Eder <daniel@deder.at>
2015-06-01 17:32:59 +0200

    #8 skeletons for map classes

---
+++ src/main/java/org/jrune/map/RuneMap.java
@@ -0,0 +1,12 @@
+package org.jrune.map;
+
+/**
+ * Represents the current status of a map. It handles everything related
+ * to this map like positioning entities and triggering actions.
+ * 
+ * @author lycis
+ *
+ */
+public class RuneMap {
+    
+}
---
+++ src/main/java/org/jrune/map/RuneMapFactory.java
@@ -0,0 +1,19 @@
+package org.jrune.map;
+
+/**
+ * This factory is used to load maps from their definition files. Basically
+ * it reads the map definition and creates a map from it.
+ * 
+ * @author lycis
+ *
+ */
+public class RuneMapFactory {
+    
+    // no constructor
+    private RuneMapFactory() {
+    }
+    
+    public static RuneMap load(String name) {
+	return null;
+    }
+}
